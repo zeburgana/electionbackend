b899a647b1fe4883930b857d58e1df939988b3ae
Martynas <martysgalkys@gmail.com>
2023-11-11 11:50:17 +0200

    Initial commit

---
+++ src/main/java/com/example/electionbackend/ElectionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.electionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ElectionBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/electionbackend/ElectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
